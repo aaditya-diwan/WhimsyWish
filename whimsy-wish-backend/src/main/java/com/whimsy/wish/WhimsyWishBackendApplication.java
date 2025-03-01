12645c0ac597a936d95da40f395b29ecb48cc717
Aaditya Diwan <aadityapdiwan@gmail.com>
2025-03-01 22:55:42 +0530

    Initial UI and Backend Setup!

---
+++ whimsy-wish-backend/src/main/java/com/whimsy/wish/WhimsyWishBackendApplication.java
@@ -0,0 +1,13 @@
+package com.whimsy.wish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhimsyWishBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhimsyWishBackendApplication.class, args);
+	}
+
+}
---
+++ whimsy-wish-backend/src/test/java/com/whimsy/wish/WhimsyWishBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whimsy.wish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhimsyWishBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
